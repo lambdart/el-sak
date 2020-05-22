27f58da280a7df85b77b113e74a373cea8dfeaa3
esac-io <esac-io@tutanota.com>
2020-05-22 20:53:03 -0300

    Add comment header and vex-load-file function

--- vex-c.el
+++ vex-c.el
@@ -1,3 +1,41 @@
+;;; vex-c.el --- vanilla extensions
+;;
+;; -*- lexical-binding: t -*-
+;;
+;; Author: esac <esac-io@tutanota.com>
+;; Version: 0.1
+;; URL: https://github.com/esac-io/vex
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; MIT License
+;;
+;; Copyright (c) 2020 esac
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;; Commentary:
+;;
+;; Basic Emacs C source functions.
+;;
+;;; Code:
+
 (defun safe-funcall (func &rest args)
   "Call FUNC with ARGS, if it's bounded."
   (when (fboundp func)
@@ -11,8 +49,6 @@
   (let ((buffer (get-buffer buffer-or-name)))
     (when (buffer-live-p buffer)
       (kill-buffer buffer))))
-
-
 
 (provide 'vex-c)
 ;;; vex-c.el ends here
--- vex-subr.el
+++ vex-subr.el
@@ -1,11 +1,49 @@
-(require 'subr)
+;;; vex-subr.el --- vanilla extension
+;;
+;; -*- lexical-binding: t -*-
+;;
+;; Author: esac <esac-io@tutanota.com>
+;; Version: 0.1
+;; URL: https://github.com/esac-io/vex
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; MIT License
+;;
+;; Copyright (c) 2020 esac
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+;;
+;;; Commentary:
+;;
+;; Basic Emacs sub process functions.
+;;
+;;; Code:
+
 (require 'files)
 
 (defun safe-start-process (name buffer program &optional args)
-  "Start a program in a subprocess."
+  "Just a `start-process' function wrapper.
+If program exists in `exec-path' execute it.
+See \\[describe-function] `start-process' for its documentation.'"
   (when (executable-find program)
-    (start-process name buffer program
-      (or args nil))))
+    (start-process name buffer program args)))
 
 (provide 'vex-subr)
 ;;; vex-subr.el ends here
